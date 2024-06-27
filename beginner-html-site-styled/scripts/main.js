25dee99b169365f403da5c551dc1e43989d20281
EggRoll <2139971764@qq.com>
2024-06-27 15:57:07 +0800

    初步学习JS

---
+++ beginner-html-site-styled/scripts/main.js
@@ -0,0 +1,11 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myvar;
+myvar = "李雷";
+// 也可以直接写let myvar = "李雷"
+
+// 可以直接通过变量名取得变量的值
+// 变量可以有不同的数据类型，比如字符串、数字、布尔值、数组、对象
+
+// 之后的一些测试工作将在浏览器的控制台里进行
