30284a4e74c55d3657567b9e95a78ea373f335f1
EggRoll <2139971764@qq.com>
2024-06-28 16:50:41 +0800

    更改了星星的透明图，现在可以实现点击图片切换了

--- beginner-html-site-styled/scripts/main.js
+++ beginner-html-site-styled/scripts/main.js
@@ -1,11 +1,10 @@
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+let myImage = document.querySelector("img");
 
-let myvar;
-myvar = "李雷";
-// 也可以直接写let myvar = "李雷"
-
-// 可以直接通过变量名取得变量的值
-// 变量可以有不同的数据类型，比如字符串、数字、布尔值、数组、对象
-
-// 之后的一些测试工作将在浏览器的控制台里进行
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/star.png");
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+};
