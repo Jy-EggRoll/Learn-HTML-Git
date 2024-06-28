3eee8d57f5c44d9d16e3105bb3f10ed8fd596a33
EggRoll <2139971764@qq.com>
2024-06-28 17:17:00 +0800

    构建完了入门级网站

--- beginner-html-site-styled/scripts/main.js
+++ beginner-html-site-styled/scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = function () {
         myImage.setAttribute("src", "images/firefox-icon.png");
     }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("请输入您的名字：");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "你好，" + myName;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = storedName + "，欢迎访问此页面";
+}
+
+myButton.onclick = function () {
+    setUserName();
+};
